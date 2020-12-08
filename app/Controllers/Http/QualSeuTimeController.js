c61f20402e661a3ba322e87d3955760f92e9e857
Argemiro Junior <argemirojunior@gmail.com>
2020-12-08 19:29:14 -0300

    EFVE

--- app/Controllers/Http/QualSeuTimeController.js
+++ app/Controllers/Http/QualSeuTimeController.js
@@ -20,7 +20,7 @@ class QualSeuTimeController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
-    const time = await Time.all()
+    const Time = await Time.all()
     return time
   }
 
