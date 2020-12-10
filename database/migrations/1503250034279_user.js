d3395d2a479a530ba2f0b2abf1c7e41c36715d2f
Argemiro Junior <argemirojunior@gmail.com>
2020-12-10 17:02:16 -0300

    Primeiro GIT

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -10,7 +10,6 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.timestamps()
       table
       .integer("time_id")
       .unsigned()
@@ -18,7 +17,7 @@ class UserSchema extends Schema {
       .inTable("times")
       .onUpdate("cascade")
       .onDelete("cascade")
-      .notNullable();
+      .nullable();
       table.timestamps()
     })
   }
